5980bff9ebfe3cb65d4e5bf344dfae6dfb960b9b
Julian George <jules104@gmail.com>
2019-08-05 06:23:20 +0100

    adding pairing practice is number even

---
+++ pairing_practice_is_number_even.rb
@@ -0,0 +1,18 @@
+def is_even?(int)
+ 	if int % 2 == 1
+ 	false
+ 	else true
+ 	end
+ end
+
+p is_even?(3) #p is_even?(3) #=> false
+p is_even?(4) #p is_even?(4) #=> true
+
+=begin
+define a method that takes an argument
+use a method to identify if argument is even or odd
+if argument is even return true
+if argument is odd return false
+close method
+call argument to method
+=end
